30c8d6e6b63a2545acc4f73f8899416b0c13eeca
HazemAhmed95 <hazem_yones95cs@hotmail.com>
2017-01-30 19:31:50 +0200

    r:refactored the code and added the messges to inform the user if an error ouccourd

--- louay/www/js/controllers.js
+++ louay/www/js/controllers.js
@@ -13,28 +13,35 @@ angular.module('starter.controllers', [])
 
 
 .controller('loginCtrl', function($scope,Auth) {
- 
+   // holds the user data
+	
 	$scope.user = {email:"",password:""}
-	$scope.$errorMessage ={email:"",password:""}
+	
+	// holds the messge erros to appear under each input 
+	
+	$scope.errorMessage ={email:"",password:""}
+	
 	$scope.emailLogin = function(){
 	//$createUserWithEmailAndPassword
+	
 		Auth.$signInWithEmailAndPassword($scope.user.email, $scope.user.password)
-        .then(function(firebaseUser) {
+			.then(function(firebaseUser) {
           
-			$scope.message = "User created with uid: " + firebaseUser.uid;
-			
-			console.log(message);
-        
+			//TODO when user is logged in
+		
 		}).catch(function(error) {
       
 			$scope.error = error;
-			
-			if(JSON.stringify($scope.error.message).includes("password"))
-				
-				$scope.$errorMessage.password=$scope.error.message;
-		else 
-				$scope.$errorMessage.email = $scope.error.message;
-        });
+			//check if the error is realted to the password if correct set $errorMessage.password to it
+			if(JSON.stringify($scope.error.message).search("password")!=-1)
+	
+				$scope.errorMessage.password=$scope.error.message;
+	      //case if the error is realted to the email
+			else 
+			$scope.errorMessage.email = $scope.error.message;
+       
+		});
+	
 	};
 
 	
