378c99e03b8d8795dc4348c035005d50fd324d0d
HazemAhmed95 <hazem_yones95cs@hotmail.com>
2017-01-31 16:24:01 +0200

    F:the instagram login is now on the magic cloud

--- louay/www/js/controllers.js
+++ louay/www/js/controllers.js
@@ -65,7 +65,7 @@ angular.module('starter.controllers', [])
 	$scope.instagramLogin = function(){
 		//Very Basic instgramlogin with the local server 
 	   //This should change will for wraper around
-		 window.open('http://localhost:8080/redirect', 'firebaseAuth');
+		 window.open('https://instgramlogin.herokuapp.com/redirect', 'firebaseAuth');
 		//TODO Route The user
 		//TODO Route after that the user to the rest of the fields in the signup process
 	}
